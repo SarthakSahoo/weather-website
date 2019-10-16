742d03b79566fd6d77a100acc78f1b0c9d837dd0
Sarthak Sahoo <sarthakkusahoo@gmail.com>
2019-10-16 10:30:36 +0530

    Add About text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-	const url = 'https://api.darksky.net/forecast/a511f133f65ece793ca4d3c43e7582e7/'+ latitude + ',' + longitude
+	const url = 'https://api.darksky.net/forecast/a511f133f65ece793ca4d3c43e7582e7/'+ latitude + ',' + longitude + '?units=si'
 
 	request({ url, json: true }, (error, { body }= {}) => {
 		if(error) {
