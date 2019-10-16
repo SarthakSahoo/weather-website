d62163f40e3155a0133f5f14429e9948adce5b7d
Sarthak Sahoo <sarthakkusahoo@gmail.com>
2019-10-16 10:44:45 +0530

    Forecast update file

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ const forecast = (latitude, longitude, callback) => {
 			callback('Unable to find location', undefined)
 		} else {
 			callback(undefined,body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degree out. There is a " +
-			body.currently.precipProbability + "% chance of rain")
+			body.currently.precipProbability + "% chance of rain. The high today is " + body.daily.data[0].temperatureHigh + " degree with a low of " +
+			body.daily.data[0].temperatureLow + " degree.")
 		}
 	})
 }
